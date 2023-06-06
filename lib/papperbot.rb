c95330895857657ccd619122caf42e61f5524eb6
santiago <santiagodelafuente2013@gmail.com>
2023-06-06 18:19:32 +0200

    Initial commit

---
+++ lib/papperbot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "papperbot/version"
+
+module Papperbot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/papperbot_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Papperbot do
+  it "has a version number" do
+    expect(Papperbot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
